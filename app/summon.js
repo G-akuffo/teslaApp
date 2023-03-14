d1089829356edbdb1db042f9ea544b3672201257
Gottfried Akuffo <gottfried.akuffo@gmail.com>
2023-03-14 00:42:33 +0000

    other route pages added

---
+++ app/summon.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Text, View, StyleSheet, Pressable } from "react-native";
+import { useRouter } from "expo-router";
+
+import { Entypo } from "@expo/vector-icons";
+
+const SummonScreen = () => {
+  const router = useRouter();
+
+  return (
+    <View style={styles.container}>
+      <Pressable onPress={() => router.back()} style={styles.back}>
+        <Entypo name="chevron-left" size={24} color="white" />
+      </Pressable>
+      <Text style={styles.welcomeText}>Welcome to the Summon Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  welcomeText: {
+    fontSize: 25,
+    fontWeight: "bold",
+  },
+  back: {
+    position: "absolute",
+    top: 50,
+    left: 25,
+    backgroundColor: "#2f3030",
+    padding: 10,
+    borderRadius: 5,
+  },
+});
+
+export default SummonScreen;
